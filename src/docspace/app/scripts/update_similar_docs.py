e0c44a6890a0914228474675f4e4874445fa0111
Nathan Dahlberg <58701810+nadahlberg@users.noreply.github.com>
2022-12-11 14:26:42 -0500

    add front page info

--- src/docspace/app/scripts/update_similar_docs.py
+++ src/docspace/app/scripts/update_similar_docs.py
@@ -1,9 +1,15 @@
 from tqdm import tqdm
+import time
 from app import *
 
 chunks = Chunk.objects.all()
 
 for chunk in tqdm(chunks):
-    chunk.get_similar_docs()
+    try:
+        chunk.get_similar_docs()
+    except:
+        chunk.delete()
+        print('error')
+        time.sleep(5)
     print(chunk.similar_docs)
 
